3fb543bb155793774d3a76c7577a882f4a2f6053
Luccas Silva <89430801+Luccas-Silva@users.noreply.github.com>
2024-03-18 17:54:13 -0300

    page Agentes(2/2)

--- valorant-app/src/app/Components/CardAgentes/page.tsx
+++ valorant-app/src/app/Components/CardAgentes/page.tsx
@@ -1,10 +1,27 @@
 import Image from "next/image";
 
+import imags from './imgs.json'
+
+import './CardAgentes.css';
+
+interface Imags {
+    src : string;
+    alt : string;
+}
+
 export default function CardAgentes() {
     return (
         <>  
-            <h1>CardAgentes</h1>
-            <Image  src="/assets/Property_Agentes/Property_Yoru.svg" width={100} height={100} alt="game play"/>
+            
+            <section className="CardsAgentes">
+            <h1><hr /> // ESCOLHA O SEU AGENTE</h1>
+                {
+                    imags.map((link:Imags) =>(
+                        <Image className="CardAgente" src={link.src} width={150} height={150} alt={link.alt}/>
+                    ))
+                }
+            </section>
+            
         </>
     );
 }
