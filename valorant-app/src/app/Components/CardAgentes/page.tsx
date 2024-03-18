ab9da29d39d24348197d70f55ee2650dbbe71c56
Luccas Silva <89430801+Luccas-Silva@users.noreply.github.com>
2024-03-18 18:11:57 -0300

    rotas agentes

--- valorant-app/src/app/Components/CardAgentes/page.tsx
+++ valorant-app/src/app/Components/CardAgentes/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
-
+import Link from "next/link";
 import imags from './imgs.json'
-
 import './CardAgentes.css';
+
+
 
 interface Imags {
     src : string;
     alt : string;
+    href : string;
 }
 
 export default function CardAgentes() {
@@ -15,9 +17,11 @@ export default function CardAgentes() {
             
             <section className="CardsAgentes">
             <h1><hr /> // ESCOLHA O SEU AGENTE</h1>
-                {
+               {
                     imags.map((link:Imags) =>(
-                        <Image className="CardAgente" src={link.src} width={150} height={150} alt={link.alt}/>
+                        <Link href={ link.href }>
+                            <Image className="CardAgente" src={link.src} width={150} height={150} alt={link.alt}/>
+                        </Link>
                     ))
                 }
             </section>
