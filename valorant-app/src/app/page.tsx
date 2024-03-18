c2109e6fa7c68eb42ab006ba64e90f54ff2aca02
Luccas Silva <89430801+Luccas-Silva@users.noreply.github.com>
2024-03-18 01:10:29 -0300

    home page (3/3)

--- valorant-app/src/app/page.tsx
+++ valorant-app/src/app/page.tsx
@@ -12,9 +12,7 @@ export default function Home() {
         <Image className="valorant" src="/assets/V_Logo_White.png" width={750} height={150} alt="Logo Valorant"/>
         <Jogarbtn/>
       </section>
-
       <h1 className="title"><hr/> NÓS SOMOS <br/> VALORANT</h1>
-
       <section className="somos">
         <div className="box-text">
           <h2>DESAFIE SEUS LIMITES</h2>
@@ -32,26 +30,22 @@ export default function Home() {
           <h1><hr/> SEUS <br/> AGENTES</h1>
           <h2>A CRIATIVIDADE É SUA MELHOR ARMA.</h2>
           <p>Mais do que armas e munição, VALORANT inclui agentes com habilidades adaptativas, rápidas e letais, que criam oportunidades para você exibir sua mecânica de tiro. Cada Agente é único, assim como os momentos de destaque de cada partida!</p>
-          
           <Link href="" target="_blank">
               <button className="btn-agentes">VER TODOS OS AGENTES</button>
           </Link>
         </div>
       </section>
 
-      <section className="agentes">
-        <div className="agente-text">
-          <h1><hr/> SEUS <br/> AGENTES</h1>
-          <h2>A CRIATIVIDADE É SUA MELHOR ARMA.</h2>
-          <p>Mais do que armas e munição, VALORANT inclui agentes com habilidades adaptativas, rápidas e letais, que criam oportunidades para você exibir sua mecânica de tiro. Cada Agente é único, assim como os momentos de destaque de cada partida!</p>
-          
+      <section className="mapas">
+        <div className="mapas-text">
+          <h1><hr/> SEUS <br/> MAPAS</h1>
+          <h2>BATALHE AO REDOR DO MUNDO</h2>
+          <p>Cada mapa serve como um palco para mostrar sua criatividade. Os mapas são feitos sob medida para estratégias de equipe, jogadas espetaculares e momentos eletrizantes. Faça as jogadas que todo mundo vai tentar imitar no futuro!</p>
           <Link href="" target="_blank">
-              <button className="btn-agentes">VER TODOS OS AGENTES</button>
+              <button>VER TODOS OS MAPAS</button>
           </Link>
         </div>
-        <Image src="/assets/Agentes.svg" width={600} height={600} alt=""/>
       </section>
-
     </>
   );
 }
