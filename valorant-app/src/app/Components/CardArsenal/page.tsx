68fa3c4038cbea0f32847836b0a89bef0ab1fc53
Luccas Silva <89430801+Luccas-Silva@users.noreply.github.com>
2024-03-19 00:29:53 -0300

    page Arsenal (2/3)

--- valorant-app/src/app/Components/CardArsenal/page.tsx
+++ valorant-app/src/app/Components/CardArsenal/page.tsx
@@ -1,15 +1,27 @@
 import Image from "next/image";
-
+import armas from "./arsenal.json";
 import './CardArsenal.css';
+
+interface Armas {
+    name : string;
+    src : string;
+    alt : string;
+    text : string;
+}
 
 export default function CardArsenal() {
     return (
         <>  
-            <section className="CardsArsenal">
-                <h1>{}</h1>
-                <Image className="" src={"/assets/Property 1=Classic.svg"} width={600} height={300} alt={""}/>
-                <p>{}</p>
-            </section>    
+            {
+                armas.map((info : Armas) =>(
+                    <section className="CardsArsenal">
+                        <h1>{info.name}</h1>
+                        <Image src={info.src} width={500} height={200} alt={info.alt} />
+                        <p>{info.text}</p>
+                    </section>
+                ))
+            }
+                
         </>
     );
 }
