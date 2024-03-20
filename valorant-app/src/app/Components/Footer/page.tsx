35c8911234524800f22b1cd8c8772c45d725a5e5
Luccas Silva <89430801+Luccas-Silva@users.noreply.github.com>
2024-03-19 22:33:31 -0300

    Footer

--- valorant-app/src/app/Components/Footer/page.tsx
+++ valorant-app/src/app/Components/Footer/page.tsx
@@ -1,10 +1,15 @@
 
+import "./Footer.css";
 
 export default function Footer() {
     return (
         <>  
             <footer>
-                <h1>Footer</h1>
+                <ul>
+                    <li>Nome: Luccas Silva - RM: 552890</li>
+                    <li>Nome: Bruno Burian - RM: 552863</li>
+                    <li>Nome: Leonardo Chaves - RM: 552707</li>
+                </ul>
             </footer>
         </>
     );
